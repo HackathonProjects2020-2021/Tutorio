69cc6b5ef76467975515975febd0ba2896a3b40b
unknown <savvychez@gmail.com>
2020-05-30 15:24:14 -0500

    gets vars from selected & hover on dashboards cars highlights them

--- public/scripts/selection.js
+++ public/scripts/selection.js
@@ -1,7 +1,15 @@
+var selected = []
 
-$( document ).ready(function() {
+$(document ).ready(function() {
     $(".card").click(function() {
         var id = $(this).attr('id');
         $('#'+id).toggleClass('selected')
     });
+    $('.button').click(function(){
+        $('.selected').each(function(i, obj) {
+            console.log(obj)
+            selected.push(obj.id)
+        });
+    });
 });
+
