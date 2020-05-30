ab38055571941f6a37aa687580453b72b9c360c5
Richard Zhang <dioxrichard@gmail.com>
2020-05-30 14:02:31 -0500

    added login and sign up functionality

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,49 +1,42 @@
 class AuthService {
     constructor() {
-        var firebaseConfig = {
-            apiKey: "AIzaSyAQRMa3o1JDpxJrA4xAk-voxHR1jIDZjtQ",
-            authDomain: "teenhackhtx-application.firebaseapp.com",
-            databaseURL: "https://teenhackhtx-application.firebaseio.com",
-            projectId: "teenhackhtx-application",
-            storageBucket: "teenhackhtx-application.appspot.com",
-            messagingSenderId: "535069797709",
-            appId: "1:535069797709:web:cf4361c71ff094b8c85b04",
-            measurementId: "G-2VGX0V4SR2"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        firebase.analytics();
-        this.auth =
-        firebase.auth();
+        this.auth = firebase.auth;
     }
     register() {
         var userEmail = document.getElementById("email").value;
         var userPass = document.getElementById("password").value;
-        this.auth.createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
 
             window.alert("Error : " + errorMessage);
+            return null;
             // ...
           });
-        
+        var user = firebase.auth().currentUser;
+        var userService = new UserSerivce();
+        userService.updateUser(user.uid, 1000);
+        return new User(user.uid, 1000);
     }
     login() {
         var userEmail = document.getElementById("email").value;
         var userPass = document.getElementById("password").value;
     
-        this.auth.signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
     
             window.alert("Error : " + errorMessage);
-    
+            return null;
             // ...
         });
+        var user = firebase.auth().currentUser;
+        var userService = new UserSerivce();
+        return new User(user.uid, userService.getPoints(user.uid));
     }
     logout() {
-        this.auth.signOut();
+        firebase.auth().signOut();
     }
 }
--- public/scripts/userservice.js
+++ public/scripts/userservice.js
@@ -1,18 +1,5 @@
 class UserSerivce {
-    constructor(subject) {
-        var firebaseConfig = {
-            apiKey: "AIzaSyAQRMa3o1JDpxJrA4xAk-voxHR1jIDZjtQ",
-            authDomain: "teenhackhtx-application.firebaseapp.com",
-            databaseURL: "https://teenhackhtx-application.firebaseio.com",
-            projectId: "teenhackhtx-application",
-            storageBucket: "teenhackhtx-application.appspot.com",
-            messagingSenderId: "535069797709",
-            appId: "1:535069797709:web:cf4361c71ff094b8c85b04",
-            measurementId: "G-2VGX0V4SR2"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        firebase.analytics();
+    constructor() {
         this.db = firebase.firestore();
         this.db.settings({ timestampsInSnapshots: true });
     }
@@ -21,6 +8,10 @@ class UserSerivce {
         await this.db.collection('users').doc(uid).set({
             points: points,
         });
+    }
+
+    async getPoints(uid) {
+        return await this.db.collection('user').doc(uid).data().points;
     }
 
     async deleteUser(uid) {
